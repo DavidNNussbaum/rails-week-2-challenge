a01d302bf60c80311090d1bc66a3d66a95624cd7
Zac <zacharyflynn06@gmail.com>
2021-03-25 14:13:56 -0600

    add category views and form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,27 @@
 class CategoriesController < ApplicationController
+
+    def new
+        @category = Category.new 
+    end
+
+    def create 
+        byebug
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to category_path(@category)
+
+        else
+            render :new
+        end
+    end
+
+    def show
+        @category = Category.find(params[:id])
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name, :party_ids, :parties_attributes => [:name, :date_time, :budget, :private])
+      end
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -3,6 +3,7 @@ class Party < ApplicationRecord
     # has_many :parties_supplies
     # has_many :supplies, through: :parties_supplies
     has_and_belongs_to_many :supplies
+
     accepts_nested_attributes_for :category
     validates :name, presence: true, uniqueness: true
 end
